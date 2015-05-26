8bc6ada1687e948dfc18020e9bcd4f4177892849
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-05-26 21:33:15 +0200

    Update angular-1 with a SW file

---
+++ angular-1-es5/README.md
@@ -0,0 +1,7 @@
+# Angular 1 with ES5
+
+## Boot the app
+
+    $ python -m SimpleHTTPServer 8000
+    // or with python 3
+    $ python3 -m http.server 8000
---
+++ angular-1-es5/sw.js
@@ -0,0 +1,84 @@
+/*eslint-disable strict */
+/* global self, importScripts, URL, caches, fetch, registration, clients, location */
+
+// Import cache polyfill
+importScripts('serviceworker-cache-polyfill.js');
+
+var staticCacheName = 'cache-static-v1';
+var gifCacheName = 'cache-gif-v1';
+var apiCacheName = 'cache-api-v1';
+var staticFiles = [
+  '/',
+  '/src/style/main.css',
+  '/vendor/angular.min.js',
+  '/vendor/angular-route.js',
+  '/src/gif/init.js',
+  '/src/gif/gif.repository.js',
+  '/src/gif/gif.service.js',
+  '/src/gif/gif.controller.js',
+  '/src/app.js',
+  'src/gif/gif.html',
+  '/offline.jpg'
+];
+
+self.addEventListener('install', function(event) {
+    console.log('Installing', event);
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('activate', event);
+});
+
+self.addEventListener('fetch', function(event) {
+    var requestURL = new URL(event.request.url);
+    console.log('Trying to fetch: ', requestURL);
+});
+
+self.addEventListener('push', function(event) {
+    console.log('Push Event Received', event);
+    console.log("use: registration.showNotification('headline', {body: 'text', icon: 'icon.png', tag: 'tag'})");
+});
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('Clicking on notification', event);
+});
+
+/**
+ * Send request -> save in cache -> reply with response
+ * If network fail => reply with cache value
+ * @param {Request} request   request from event
+ * @param {String} cacheName cache name
+ * @return {Promise}
+ */
+function networkFallbackOnCache(request, cacheName) {
+    return caches.open(cacheName).then(function(cache) {
+        return cache.match(request.clone()).then(function(response) {
+            return fetch(request.clone())
+                .then(function(networkResponse) {
+                    cache.put(request, networkResponse.clone());
+                    return networkResponse;
+                })
+                .catch(function() {
+                    return response;
+                });
+        });
+    });
+}
+
+/**
+ * Check if we have the request in cache, if we do: response, else make a network request and update the cache.
+ * @param  {Request} request
+ * @param  {String} cacheName
+ * @return {Promise}
+ */
+function cacheFallbackOnNetwork(request, cacheName) {
+    return caches.open(cacheName).then(function(cache) {
+        return cache.match(request.clone()).then(function(response) {
+            var fetchPromise = fetch(request.clone()).then(function(networkResponse) {
+                cache.put(request, networkResponse.clone());
+                    return networkResponse;
+                });
+            return response || fetchPromise;
+        });
+    });
+}
